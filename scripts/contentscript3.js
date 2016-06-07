4114c1ec5619b2bd7c7cc2a180e4eeda4e7356f0
yoshinoshabby <shabby2011@gmail.com>
2016-06-07 16:38:59 +0800

    update contentscript3.js

--- scripts/contentscript3.js
+++ scripts/contentscript3.js
@@ -365,7 +365,9 @@ var Client = {
 	'_handle_result_multi': function() {
 	    if ((window.localStorage.getItem('master') == 'true') || (window.localStorage.getItem('coopraid') == 'true')) {
 			var bbc = setInterval(function() {
+			    console.log("get in bbc!!!!!!!!!");
 				if ($('.btn-control').length > 0) {
+				    console.log("close!!!!!!!!!!!!");
 					self.click(".btn-control")
 				} else if($('.btn-usual-close').length > 0){
 					self.click(".btn-usual-close")
@@ -373,6 +375,7 @@ var Client = {
 					self.click(".btn-usual-ok:eq(0)")
 					
 				}	 else {
+				    console.log("do nothing!!!!!!!!!");
 					location.href = "http://gbf.game.mbga.jp/#coopraid";
 					clearInterval(bbc)
 				}
